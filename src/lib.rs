dd8f8c02f605e274208ff03bec576f05a0e7ba8a
divinerapier <poriter.coco@gmail.com>
2019-09-07 16:10:11 +0800

    use c functions as callback

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,27 @@ use nvml_sys::bindings::nvmlDeviceGetCudaComputeCapability;
 use nvml_sys::bindings::nvmlDeviceGetHandleByIndex_v2;
 use nvml_sys::bindings::nvmlDeviceGetMaxPcieLinkGeneration;
 use nvml_sys::bindings::nvmlDeviceGetMaxPcieLinkWidth;
+use nvml_sys::bindings::nvmlDeviceGetMemoryInfo;
+use nvml_sys::bindings::nvmlDeviceGetMinorNumber;
 use nvml_sys::bindings::nvmlDeviceGetName;
 use nvml_sys::bindings::nvmlDeviceGetPciInfo_v3;
+use nvml_sys::bindings::nvmlDeviceGetPowerManagementLimit;
 use nvml_sys::bindings::nvmlDeviceGetUUID;
 use nvml_sys::bindings::nvmlDevice_t;
+use nvml_sys::bindings::nvmlMemory_t;
 use nvml_sys::bindings::nvmlPciInfo_t;
 use nvml_sys::bindings::nvmlReturn_enum_NVML_ERROR_NOT_SUPPORTED;
 use nvml_sys::bindings::nvmlReturn_enum_NVML_SUCCESS;
+use nvml_sys::bindings::nvmlReturn_t;
 use nvml_sys::bindings::nvmlSystemGetDriverVersion;
 use nvml_sys::bindings::NVML_DEVICE_NAME_BUFFER_SIZE;
 use nvml_sys::bindings::NVML_DEVICE_UUID_BUFFER_SIZE;
 use nvml_sys::bindings::NVML_SYSTEM_DRIVER_VERSION_BUFFER_SIZE;
+
+// If you want to use the function of c as the callback function of rust,
+// the function type must be marked as unsafe extern "C"
+type ProcessOneInterger =
+    unsafe extern "C" fn(*mut nvml_sys::bindings::nvmlDevice_st, *mut u32) -> u32;
 
 pub struct NVML;
 
@@ -64,6 +74,14 @@ impl NVML {
     }
 }
 
+impl Drop for NVML {
+    fn drop(&mut self) {
+        unsafe {
+            nvml_sys::bindings::nvmlShutdown_dl();
+        }
+    }
+}
+
 pub fn result_error(result: nvml_sys::bindings::nvmlReturn_t) -> Option<String> {
     unsafe {
         if result == nvml_sys::bindings::nvmlReturn_enum_NVML_SUCCESS {
@@ -81,26 +99,119 @@ pub struct PCIInfo {
     pub bandwidth: u64,
 }
 
-pub struct Device {
-    pub handler: nvml_sys::bindings::nvmlDevice_t,
-    pub name: String,
-    pub uuid: String,
-    pub model: String,
-    pub path: String,
-    pub minor_number: i32,
-    pub power_management_limit: i32,
-    pub memory_info: String,
+pub struct ClockInfo {
+    pub cores: u64,
+    pub memory: u64,
+}
 
-    pub pci_info: PCIInfo,
-    pub bar1_memory_info: String,
-    pub max_pcie_link_generation: String,
-    pub max_pcie_link_width: String,
-    pub max_clock_info: String,
-    pub cuda_compute_capability: String,
+pub struct P2PLink {
+    pub bus_id: String,
+    pub link: P2PLinkType,
+}
+
+pub enum P2PLinkType {
+    P2PLinkUnknown = 0,
+    P2PLinkCrossCPU = 1,
+    P2PLinkSameCPU = 2,
+    P2PLinkHostBridge = 3,
+    P2PLinkMultiSwitch = 4,
+    P2PLinkSingleSwitch = 5,
+    P2PLinkSameBoard = 6,
+    SingleNVLINKLink = 7,
+    TwoNVLINKLinks = 8,
+    ThreeNVLINKLinks = 9,
+    FourNVLINKLinks = 10,
+    FiveNVLINKLinks = 11,
+    SixNVLINKLinks = 12,
+}
+
+pub struct CudaComputeCapabilityInfo {
+    pub major: u64,
+    pub minor: u64,
+}
+
+pub struct Device {
+    pub handler: Handler,
+    pub uuid: String,
+    pub path: String,
+    pub model: String,
+    pub power: Option<u64>,
+    pub memory: Option<u64>,
+    pub cpu_affinity: Option<u64>,
+    pub pci: PCIInfo,
+    pub clocks: ClockInfo,
+    pub topology: Vec<i8>,
+    pub cuda_compute_capability: CudaComputeCapabilityInfo,
 }
 
 impl Device {
-    pub fn new(index: i32) -> Result<Option<Self>, String> {
+    pub fn new(index: u32) -> Result<Self, String> {
+        let handler = Handler::new(index)?;
+        let model = handler.get_name()?;
+        let uuid = handler.get_uuid()?;
+        let minor_count = handler.get_minor_number()?;
+        let power = handler.get_power_management_limit()?;
+        let memory_info = handler.get_memory_info()?;
+        let bus_id = handler.get_pci_info()?;
+        let (bar1, _) = handler.get_bar1_memory_info()?;
+        let pcig = handler.get_max_pcie_link_generation()?;
+        let pciw = handler.get_max_pcie_link_width()?;
+        let (cores, mem) = handler.get_clock_info()?;
+        let (major, minor) = handler.get_cuda_compute_capability()?;
+        let node = Self::numa_node(&bus_id)?;
+        Ok(Device {
+            handler,
+            uuid,
+            path: format!("/dev/nvidia{}", minor_count),
+            model,
+            power: Some(power),
+            memory: Some(memory_info.total),
+            cpu_affinity: Some(0),
+            pci: PCIInfo {
+                bus_id,
+                bar1,
+                bandwidth: Self::pci_bandwidth(pcig, pciw),
+            },
+            clocks: ClockInfo { cores, memory: mem },
+            topology: vec![],
+            cuda_compute_capability: CudaComputeCapabilityInfo { minor, major },
+        })
+    }
+
+    fn numa_node(bus_id: &str) -> Result<u64, String> {
+        let filepath = format!("/sys/bus/pci/devices/{}/numa_node", bus_id.to_lowercase());
+        match std::fs::read_to_string(&filepath) {
+            Ok(content) => match content.parse() {
+                Ok(node) => {
+                    if node < 0 {
+                        return Ok(0);
+                    }
+                    return Ok(node);
+                }
+                Err(e) => return Err(format!("{}", e)),
+            },
+            Err(e) => return Err(format!("{}", e)),
+        };
+        Ok(0)
+    }
+    fn pci_bandwidth(gen: u64, width: u64) -> u64 {
+        width
+            * match gen {
+                1 => 250,
+                2 => 500,
+                3 => 985,
+                4 => 1969,
+                _ => 0,
+            }
+    }
+}
+
+pub struct Handler {
+    pub dev: nvmlDevice_t,
+}
+
+impl Handler {
+    pub fn new(index: u32) -> Result<Handler, String> {
         unsafe {
             let mut dev: nvmlDevice_t = std::ptr::null_mut();
             let result = nvmlDeviceGetHandleByIndex_v2(
@@ -110,32 +221,14 @@ impl Device {
             if result != nvmlReturn_enum_NVML_SUCCESS {
                 return Err(result_error(result).unwrap());
             }
-            // get name deviceGetName
-
-            // get uuid deviceGetUUID
-
-            // get minor number deviceGetMinorNumber
-
-            // deviceGetPowerManagementLimit
-            // deviceGetMemoryInfo
-            // deviceGetPciInfo
-            // deviceGetBAR1MemoryInfo
-            // deviceGetMaxPcieLinkGeneration
-            // deviceGetMaxPcieLinkWidth
-            // deviceGetMaxClockInfo
-            // deviceGetCudaComputeCapability
-            // numaNode
-
-            Ok(None)
+            Ok(Handler { dev })
         }
     }
-
     pub fn get_name(&self) -> Result<String, String> {
         unsafe {
             let mut name: [::std::os::raw::c_char; NVML_DEVICE_NAME_BUFFER_SIZE as usize] =
                 [0; NVML_DEVICE_NAME_BUFFER_SIZE as usize];
-            let result =
-                nvmlDeviceGetName(self.handler, &mut name[0], NVML_DEVICE_NAME_BUFFER_SIZE);
+            let result = nvmlDeviceGetName(self.dev, &mut name[0], NVML_DEVICE_NAME_BUFFER_SIZE);
             if result == nvmlReturn_enum_NVML_ERROR_NOT_SUPPORTED {
                 return Err(String::from("not supported"));
             }
@@ -152,8 +245,7 @@ impl Device {
         unsafe {
             let mut name: [::std::os::raw::c_char; NVML_DEVICE_UUID_BUFFER_SIZE as usize] =
                 [0; NVML_DEVICE_UUID_BUFFER_SIZE as usize];
-            let result =
-                nvmlDeviceGetUUID(self.handler, &mut name[0], NVML_DEVICE_UUID_BUFFER_SIZE);
+            let result = nvmlDeviceGetUUID(self.dev, &mut name[0], NVML_DEVICE_UUID_BUFFER_SIZE);
             if result == nvmlReturn_enum_NVML_ERROR_NOT_SUPPORTED {
                 return Err(String::from("not supported"));
             }
@@ -166,12 +258,11 @@ impl Device {
             Err(result_error(result).unwrap())
         }
     }
+
     pub fn get_pci_info(&self) -> Result<String, String> {
         unsafe {
             let mut pci_info: nvmlPciInfo_t = std::mem::uninitialized();
-            // let mut pci_info: std::mem::MaybeUninit<nvmlPciInfo_t> =
-            //     std::mem::MaybeUninit::<nvmlPciInfo_t>::uninit();
-            let result = nvmlDeviceGetPciInfo_v3(self.handler, &mut pci_info as *mut nvmlPciInfo_t);
+            let result = nvmlDeviceGetPciInfo_v3(self.dev, &mut pci_info as *mut nvmlPciInfo_t);
             if result == nvmlReturn_enum_NVML_ERROR_NOT_SUPPORTED {
                 return Err(String::from("not supported"));
             }
@@ -191,7 +282,7 @@ impl Device {
         unsafe {
             let mut bar1_memory_info: nvmlBAR1Memory_t = std::mem::uninitialized();
             let result = nvmlDeviceGetBAR1MemoryInfo(
-                self.handler,
+                self.dev,
                 &mut bar1_memory_info as *mut nvmlBAR1Memory_t,
             );
             if result == nvmlReturn_enum_NVML_ERROR_NOT_SUPPORTED {
@@ -207,46 +298,12 @@ impl Device {
         }
     }
 
-    pub fn get_max_pcie_link_generation(&self) -> Result<u64, String> {
-        unsafe {
-            let mut link: ::std::os::raw::c_uint = 0;
-            let result = nvmlDeviceGetMaxPcieLinkGeneration(
-                self.handler,
-                &mut link as *mut ::std::os::raw::c_uint,
-            );
-            if result == nvmlReturn_enum_NVML_ERROR_NOT_SUPPORTED {
-                return Err(String::from("not supported"));
-            }
-            if result == nvmlReturn_enum_NVML_SUCCESS {
-                return Ok(link as u64);
-            }
-            Err(result_error(result).unwrap())
-        }
-    }
-
-    pub fn get_max_pcie_link_width(&self) -> Result<u64, String> {
-        unsafe {
-            let mut width: ::std::os::raw::c_uint = 0;
-            let result = nvmlDeviceGetMaxPcieLinkWidth(
-                self.handler,
-                &mut width as *mut ::std::os::raw::c_uint,
-            );
-            if result == nvmlReturn_enum_NVML_ERROR_NOT_SUPPORTED {
-                return Err(String::from("not supported"));
-            }
-            if result == nvmlReturn_enum_NVML_SUCCESS {
-                return Ok(width as u64);
-            }
-            Err(result_error(result).unwrap())
-        }
-    }
-
     pub fn get_clock_info(&self) -> Result<(u64, u64), String> {
         unsafe {
             let mut sm: ::std::os::raw::c_uint = 0;
             let mut mem: ::std::os::raw::c_uint = 0;
             let mut result = nvmlDeviceGetClockInfo(
-                self.handler,
+                self.dev,
                 nvmlClockType_enum_NVML_CLOCK_SM,
                 &mut sm as *mut ::std::os::raw::c_uint,
             );
@@ -255,7 +312,7 @@ impl Device {
             }
             if result == nvmlReturn_enum_NVML_SUCCESS {
                 result = nvmlDeviceGetClockInfo(
-                    self.handler,
+                    self.dev,
                     nvmlClockType_enum_NVML_CLOCK_MEM,
                     &mut mem as *mut ::std::os::raw::c_uint,
                 );
@@ -274,7 +331,7 @@ impl Device {
             let mut major: ::std::os::raw::c_int = 0;
             let mut minor: ::std::os::raw::c_int = 0;
             let result = nvmlDeviceGetCudaComputeCapability(
-                self.handler,
+                self.dev,
                 &mut major as *mut ::std::os::raw::c_int,
                 &mut minor as *mut ::std::os::raw::c_int,
             );
@@ -284,17 +341,58 @@ impl Device {
             return Ok((major as u64, minor as u64));
         }
     }
-    pub fn numa_node(&self) -> Result<u64, String> {
-        Ok(0)
+
+    pub fn get_memory_info(&self) -> Result<nvmlMemory_t, String> {
+        unsafe {
+            let mut mem: nvmlMemory_t = std::mem::uninitialized();
+            let mut result = nvmlDeviceGetMemoryInfo(self.dev, &mut mem as *mut nvmlMemory_t);
+            if result == nvmlReturn_enum_NVML_ERROR_NOT_SUPPORTED {
+                return Err(String::from("not supported"));
+            }
+            if result != nvmlReturn_enum_NVML_SUCCESS {
+                return Err(result_error(result).unwrap());
+            }
+            Ok(mem)
+        }
+    }
+
+    pub fn get_minor_number(&self) -> Result<u64, String> {
+        unsafe { self.get_one_interger(nvmlDeviceGetMinorNumber) }
+    }
+
+    pub fn get_power_management_limit(&self) -> Result<u64, String> {
+        unsafe { self.get_one_interger(nvmlDeviceGetPowerManagementLimit) }
+    }
+
+    pub fn get_max_pcie_link_generation(&self) -> Result<u64, String> {
+        unsafe { self.get_one_interger(nvmlDeviceGetMaxPcieLinkGeneration) }
+    }
+
+    pub fn get_max_pcie_link_width(&self) -> Result<u64, String> {
+        unsafe { self.get_one_interger(nvmlDeviceGetMaxPcieLinkWidth) }
     }
 }
 
-impl Drop for NVML {
-    fn drop(&mut self) {
+impl Handler {
+    extern "C" fn get_one_interger(&self, f: ProcessOneInterger) -> Result<u64, String> {
         unsafe {
-            nvml_sys::bindings::nvmlShutdown_dl();
+            let mut n: ::std::os::raw::c_uint = 0;
+            let result = f(self.dev, &mut n as *mut ::std::os::raw::c_uint);
+            if result == nvmlReturn_enum_NVML_ERROR_NOT_SUPPORTED {
+                return Err(String::from("not supported"));
+            }
+            if result == nvmlReturn_enum_NVML_SUCCESS {
+                return Ok(n as u64);
+            }
+            Err(result_error(result).unwrap())
         }
     }
 }
 
-// fn shutdown() {}
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test() {
+        let nvml = super::NVML::new().unwrap();
+    }
+}
